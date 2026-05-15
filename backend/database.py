83a4e92e88c1d0800ac5c3b3be304a9a925b5c61
VinayakRY <110305578+VinayakRY@users.noreply.github.com>
2026-05-15 21:57:25 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-
-DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:"
-    f"{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
