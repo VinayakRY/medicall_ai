dee5fb97519cb89d14df906d4829e55455331af9
Demo User <demo@example.com>
2026-06-20 16:10:38 +0530

    Use SQLite for deployment

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-load_dotenv()
+DATABASE_URL = "sqlite:///./medicall.db"
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-
-DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:"
-    f"{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
-
-engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
